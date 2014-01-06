543105092b861703841048f794ca9cfa9d5c0974
Charles Hunt <charlesalberthunt@gmail.com>
2014-01-05 21:06:33 -0600

    add heroku mongo db

--- app/utils/DatabaseService.scala
+++ app/utils/DatabaseService.scala
@@ -6,10 +6,13 @@ import com.mongodb.casbah.Imports._
 trait DatabaseService {
 
   //todo: move connection information into application.conf
-  val mongoClient = MongoClient("localhost", 27017)
+//  val mongoClient = MongoClient("localhost", 27017)
+  val mongoConn = MongoConnection("ds061238.mongolab.com", 61238)
 
   def getCollection(collectionName: String): MongoCollection = {
-    val db = mongoClient("logicdb")
+//    val db = mongoClient("logicdb")
+    val db = mongoConn("heroku_app20997644")
+    mongoConn.authenticate("logicuser", "philo123")
     // Gets a reference to the collection
     // By default, you get a BSONCollection.
     db(collectionName)
