744a60b9da037f50ac6389a3977ce0f33cf46ae1
Charles Hunt <charlesalberthunt@gmail.com>
2014-02-26 01:18:10 -0600

    aggregation for errors and js chart wire up

--- app/controllers/Metrics.scala
+++ app/controllers/Metrics.scala
@@ -6,21 +6,43 @@ import com.mongodb.casbah.commons.MongoDBObject
 object Metrics extends Controller with Access {
 
   def index = Action {  implicit request =>
-    Ok(views.html.metrics(loginForm, registerForm))
+    val solutions = errorMetrics()
+    Ok(views.html.metrics(loginForm, registerForm, solutions.apply(0),solutions.apply(1)))
   }
 
-  def errorMetrics() = {
-
+  def errorMetrics():Seq[String] = {
+    var solutions: Seq[String] = Seq()
     val coll = getCollection("logs")
 
     val results = coll.aggregate(
-      List( MongoDBObject("$match" -> MongoDBObject("level" -> "ERROR")),
-        MongoDBObject("$unwind" -> "$tags"),
-        MongoDBObject("$group" -> MongoDBObject("_id" -> "$tags", "authors" -> MongoDBObject("$addToSet" -> "$author")))
+      List(  MongoDBObject("$project" -> MongoDBObject("_id" -> 0 ,"level" -> 1, "date" -> 1)),
+        MongoDBObject("$match" -> MongoDBObject("level" -> "ERROR")),
+        MongoDBObject("$sort" -> MongoDBObject("date" -> -1))
       )
     )
 
+    val m = scala.collection.mutable.Map[Any,Int]()
+    val increment = (x: Int) => x + 1
 
+    results.results.foreach(x =>
+      if(m.contains(x.get("date"))) {
+        m.put(x.get("date"),increment(m.get(x.get("date")).get))
+      }
+      else m.put(x.get("date"),1)
+    )
+
+    val dates: StringBuilder = StringBuilder.newBuilder
+    val numbers: StringBuilder = StringBuilder.newBuilder
+    m.foreach{x =>
+      dates.append(x._1.toString).append(",")
+      numbers.append(x._2+ ",")
+    }
+
+    solutions = solutions :+ dates.deleteCharAt(dates.length-1).toString
+    solutions = solutions :+ numbers.deleteCharAt(numbers.length-1).toString
+    println(dates)
+    println(numbers)
+    solutions
   }
 
 }
